8d016ab6c5bad922299d3a814c8379d485057321
David Kaye <david@ethernull.org>
2014-03-09 18:00:54 +0100

    compile using the MinGW branch of Qt

--- compile_all.bat
+++ compile_all.bat
@@ -1,18 +1,7 @@
 @echo off
 
-call "%VS100COMNTOOLS%\vsvars32.bat"
+set PATH=C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin;C:\Qt\Qt5.2.1/Tools/mingw48_32\bin;%PATH%
 
-cl.exe /DWINDOWS /c utilities.c
-
-cl.exe /DWINDOWS bnpts2npts.c utilities.obj
-cl.exe /DWINDOWS data_size.c utilities.obj
-
-cl.exe /DWINDOWS gen_analytic.c utilities.obj
-cl.exe /DWINDOWS noff2npts.c utilities.obj
-
-cl.exe /DWINDOWS scale.c utilities.obj
-cl.exe /DWINDOWS shift_data.c utilities.obj
-cl.exe /DWINDOWS swap_yz.c utilities.obj
-cl.exe /DWINDOWS ysort.c utilities.obj
+mingw32-make.exe
 
 pause
