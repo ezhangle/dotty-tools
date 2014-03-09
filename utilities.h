3cac8eb517e952c187d8958e22b15b3294e57f9c
David Kaye <david@ethernull.org>
2014-03-09 17:59:51 +0100

    Synchonise some utilities.h changes - i.e. add vector definitions and an enum

--- utilities.h
+++ utilities.h
@@ -5,3 +5,12 @@ int detect_normals(FILE *fp);
 void open_file(FILE **fp
 		, char *filename
 		, char *mode);
+
+enum { X_Axis, Y_Axis, Z_Axis };
+
+typedef struct { double x, y, z; } vector;
+typedef struct { double eval; vector evec; } evector;
+
+double angle( vector A, vector B );
+vector cross_product(vector A, vector B);
+int evec_comp(const void *one, const void *two);
